8cee7358c47cf09fd708933587f777200c69007d
hathaw9y <iamjames1217@gmail.com>
2025-07-23 09:54:03 +0900

    Remove .DS_Store from repository

--- HDLBits/Circuits/Finite_State_Machine/Fsm_serialdp.v
+++ HDLBits/Circuits/Finite_State_Machine/Fsm_serialdp.v
@@ -4,39 +4,43 @@ module top_module(
     input reset,    // Synchronous reset
     output [7:0] out_byte,
     output done
-); 
-    reg [3:0] state, next_state;
-    parameter IDLE = 0, START =1, S1 = 2, S2 = 3, S3 = 4, S4 = 5, S5 = 6, S6 = 7, S7 = 8, S8 = 9, STOP = 10, SUCC = 11, ERR = 12, PAR = 13;
+); //
+
+    // Modify FSM and datapath from Fsm_serialdata
+    reg [2:0] state, next_state;
+    parameter IDLE = 0, DATA = 1, STOP = 2, DONE = 3, ERR = 4;
     reg odd;
+
+    parity par1(clk, ~(state == DATA), in, odd);
+
+    integer cnt;
+
     always @(*) begin
         case (state)
-            IDLE: next_state = (in) ? IDLE : START;
-            START: next_state = S1;
-            S1: next_state = S2;
-            S2: next_state = S3;
-            S3: next_state = S4;
-            S4: next_state = S5;
-            S5: next_state = S6;
-            S6: next_state = S7;
-            S7: next_state = PAR;
-            PAR: next_state = (in != odd) ? STOP : ERR;
-            STOP: next_state = (in) ? SUCC : ERR;
-            SUCC: next_state = (in) ? IDLE : START;
+            IDLE: next_state = (~in) ? DATA : IDLE;
+            DATA: next_state = (cnt == 9 && in == ~odd) ? STOP : (cnt == 9 && in == odd) ? ERR : DATA;
+            STOP: next_state = (in) ? DONE : ERR;
+            DONE: next_state = (~in) ? DATA : IDLE;
             ERR: next_state = (in) ? IDLE : ERR;
         endcase
     end
 
     always @(posedge clk) begin
-        if (reset)
+        if (reset) begin
             state <= IDLE;
-        else begin
+            cnt <= 0;
+        end else begin
             state <= next_state;
-            if (state > 0 & state <9) 
-                out_byte[state -1] = in;
+            if (next_state == DATA) begin
+                cnt = cnt + 1;
+                if (cnt > 1)
+                    out_byte[cnt - 2] <= in;
+            end else if (next_state == DONE || next_state == ERR) begin
+                cnt = 0;
+            end
         end
     end
 
-    parity par1 (clk, reset, in, odd);
-    assign done = (state == SUCC);
+    assign done = (state == DONE);
 
 endmodule
